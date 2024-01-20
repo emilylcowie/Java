de7bd688f28903e7a4a36cf15fc00a27073bcd96
Emily Cowie <emilylcowie@gmail.com>
2024-01-20 22:56:03 +0000

    .

---
+++ Learning Java Book/Chapter 1/Welcome1_1.java
@@ -0,0 +1,6 @@
+public class Welcome1_1 { //class block
+    public static void main(String[] args) { // Method block
+        // Display message Welcome to Java! on the console
+        System.out.println("Welcome to Java!");
+    }
+}
---
+++ Learning Java Book/Chapter 1/WelcomeWithThreeMessages1_2.java
@@ -0,0 +1,7 @@
+public class WelcomeWithThreeMessages1_2 {
+    public static void main(String[] args) {
+        System.out.println("Programming is fun!");
+        System.out.println("Fundamentals First");
+        System.out.println("Problem Driven");
+    }
+}
